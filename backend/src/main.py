417733f505fb60e5d0584a78f1e37ca0364d6bd7
twpro8 <twpro8@gmail.com>
2026-02-15 21:35:47 +0300

    chore: add cors middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,7 @@ Main module.
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import todo
 from src.api import user
@@ -22,5 +23,15 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(todo.router)
 app.include_router(user.router)
